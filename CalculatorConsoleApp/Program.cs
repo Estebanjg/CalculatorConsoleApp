38738b01006143eb78bd7be8286a600018058848
Estebanjg <estebanjg34@gmail.com>
2025-05-22 17:06:00 -0400

    Initial commit

---
+++ CalculatorConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CalculatorConsoleApp
+{
+    class CalculatorConsoleApp{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, This is the Console Calculator!");
+            
+            Console.ReadKey(); //Press any key to close
+        }
+        
+    }
+    
+}
