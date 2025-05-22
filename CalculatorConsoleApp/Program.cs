d68ca5a1fa7c8b80e070d54ab6ffb73734f80e42
Estebanjg <estebanjg34@gmail.com>
2025-05-22 19:22:25 -0400

    Added Basic Functionality to the Calculator

--- CalculatorConsoleApp/Program.cs
+++ CalculatorConsoleApp/Program.cs
@@ -4,7 +4,57 @@ namespace CalculatorConsoleApp
     class CalculatorConsoleApp{
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            int result = 0;
+            char operation;
+            Boolean IO = true;
+            
+            
+            
             Console.WriteLine("Hello, This is the Console Calculator!");
+            Console.WriteLine("Please enter the first number: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine()); //Casts 1st input (String) to Int
+            
+            Console.WriteLine("Please enter the second number: ");
+            
+            num2 = Convert.ToInt32(Console.ReadLine()); //Casts 2nd input (String) to Int
+
+            Console.WriteLine("What operation would you like to perform? ");
+            Console.WriteLine("Please enter the operation symbol: ");
+
+            operation = Convert.ToChar(Console.ReadLine());
+
+            while(IO == true)
+            {
+                if (operation == '+')
+                {
+                    result = num1 + num2;
+                    IO = false;
+                }
+                else if (operation == '-')
+                {
+                    result = num1 - num2;
+                    IO = false;
+                }
+                else if (operation == '*')
+                {
+                    result = num1 * num2;
+                    IO = false;
+                }
+                else if (operation == '/')
+                {
+                    result = num1 / num2;
+                    IO = false;
+                }
+                else
+                {
+                    IO = true;
+                }    
+            }
+
+            Console.WriteLine("The result: " + result);
             
             Console.ReadKey(); //Press any key to close
         }
--- CalculatorConsoleApp/obj/Debug/net8.0/CalculatorConsoleApp.AssemblyInfo.cs
+++ CalculatorConsoleApp/obj/Debug/net8.0/CalculatorConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38738b01006143eb78bd7be8286a600018058848")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
